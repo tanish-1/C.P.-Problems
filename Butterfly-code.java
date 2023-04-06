a7f1b93d554690db55327fbf54f8c700b772c6c6
Tanish <94254649+tanish-1@users.noreply.github.com>
2023-04-06 14:37:16 +0530

    Create Butterfly-code.java

---
+++ Butterfly-code.java
@@ -0,0 +1,34 @@
+# Butterfly-code
+public class Pattern
+{
+	public static void main(String[] args) {
+	    int n=5;
+	  //  int m = 5;
+	    for(int i =1 ; i<=n;i++){
+	        for(int j = 1 ; j <= i ; j++){
+	              System.out.print("*");
+	        }
+	             for(int j=1;j<=2*(n-i);j++){
+	                   System.out.print(" ");
+	              }
+	              for(int j=1;j<=i;j++){
+	                System.out.print("*");
+	              }
+
+	          System.out.println();
+	    }
+	     for(int i = n ; i>=1;i--){
+	        for(int j = 1 ; j <= i ; j++){
+	              System.out.print("*");
+	        }
+	             for(int j=1;j<=2*(n-i);j++){
+	                   System.out.print(" ");
+	              }
+	              for(int j=1;j<=i;j++){
+	                System.out.print("*");
+	              }
+
+	          System.out.println();
+	    }
+	}
+}
